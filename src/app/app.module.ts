ebd549dd4c26a073c27fce18fbed5c98045c656d
LAPTOP-MJICMDF2\Tharun <tharundillikar@gmail.com>
2017-07-14 01:19:02 +0530

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdInputModule } from '@angular/material';
+import { MdSelectModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './sign/landing-page/landing-page.component';
 import { SignComponent } from './sign/sign.component';
 import { SignInComponent } from './sign/sign-in/sign-in.component';
-
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { StudentComponent } from './dashboard/student/student.component';
 import { AdminComponent } from './dashboard/admin/admin.component';
 import { TrainerComponent } from './dashboard/trainer/trainer.component';
@@ -28,28 +31,31 @@ import { TrainerComponent } from './dashboard/trainer/trainer.component';
     ReactiveFormsModule,
     FormsModule,
     HttpModule,
+    MdSelectModule,
+    MdInputModule,
+    BrowserAnimationsModule,
       RouterModule.forRoot([
       { path: '', redirectTo: 'sign', pathMatch: 'full' },
 
       {
         path: 'sign', component: SignComponent,
-          children: [
-          { path: '', redirectTo: 'signin', pathMatch: 'full' },
+        children: [
+          { path: '', redirectTo: 'landingPage', pathMatch: 'full' },
           { path: 'signin', component: SignInComponent },
           { path: 'landingPage', component: LandingPageComponent }
         ]
       },
       {
         path: 'admin', component: AdminComponent,
-        
+
       },
       {
         path: 'student', component: StudentComponent,
-        
+
       },
       {
         path: 'trainer', component: TrainerComponent,
-        
+
       }
     ])
   ],
--- src/app/sign/landing-page/landing-page.component.ts
+++ src/app/sign/landing-page/landing-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
+  submitText = 'Submit'
   constructor() { }
 
   ngOnInit() {
